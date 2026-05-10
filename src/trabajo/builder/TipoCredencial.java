1debb7a4b53377ca5f0c80910d0f6294a46e7d1c
EsePapuMisterioso8 <elpapafrancisco88@gmail.com>
2026-05-09 21:13:03 -0600

    comentarioTipo

--- src/trabajo/builder/TipoCredencial.java
+++ src/trabajo/builder/TipoCredencial.java
@@ -1,5 +1,8 @@
 package trabajo.builder;
 
 public enum TipoCredencial {
+    // ---------------------------------
+    // Código para dar los tipos de credenciales 
+    // ---------------------------------
     CREDENCIAL_ALUMNO, CREDENCIAL_DOCENTE
 }
