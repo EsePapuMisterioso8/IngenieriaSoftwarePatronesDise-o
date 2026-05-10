b15125deca68490a85751dd3fee76bf570b58fe6
EsePapuMisterioso8 <elpapafrancisco88@gmail.com>
2026-05-09 20:04:11 -0600

    comentarios Credencial

--- src/trabajo/builder/Credencial.java
+++ src/trabajo/builder/Credencial.java
@@ -1,6 +1,9 @@
 package trabajo.builder;
 
 public class Credencial {
+
+    // Variables de la clase y se declaran final para que no sean
+    // modificados despues
     private final TipoCredencial tipo;
     private final String nombre;
     private final String foto;
@@ -10,6 +13,7 @@ public class Credencial {
     private final String firma;
     private final String carrera;
 
+    // constructor con parametros y recibe todos los parametros
     public Credencial(TipoCredencial tipo, String nombre, String foto, String numeroCuenta, String numeroTrabajador,
                       String fechaExpiración, String firma, String carrera){
         this.tipo = tipo;
@@ -22,34 +26,33 @@ public class Credencial {
         this.carrera = carrera;
     }
 
+    // método para obtener el tipo de credencial desde una instancia
     public TipoCredencial getTipo() {
         return tipo;
     }
-
+    // método para obtener el nombre desde una instancia
     public String getNombre() {
         return nombre;
     }
-
+    // método para obtener la foto desde una instancia
     public String getFoto() {
         return foto;
     }
-
+    // método para obtener el núm de cuenta desde una instancia
     public String getNumeroCuenta() {
         return numeroCuenta;
     }
-
-    public String getNumeroTrabajador() {
-        return numeroTrabajador;
-    }
-
+    // método para obtener el núm de trabajador desde una instancia
+    public String getNumeroTrabajador() {return numeroTrabajador;}
+    // método para obtener la fecha de expiración desde una instancia
     public String getFechaExpiración() {
         return fechaExpiración;
     }
-
+    // método para obtener la firma desde una instancia
     public String getFirma() {
         return firma;
     }
-
+    // método para obtener la carrera desde una instancia
     public String getCarrera() {
         return carrera;
     }
