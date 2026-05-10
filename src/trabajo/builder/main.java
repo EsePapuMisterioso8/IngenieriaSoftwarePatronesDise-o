59e34316079cf163a2c41f5e3d336d4d2457a4ee
EsePapuMisterioso8 <elpapafrancisco88@gmail.com>
2026-05-09 21:16:34 -0600

    comentarioMainBuilder

--- src/trabajo/builder/main.java
+++ src/trabajo/builder/main.java
@@ -2,18 +2,28 @@ package trabajo.builder;
 
 public class main {
     public static void main(String[] args) {
+
+        // ---------------------------------
+        // Clase principal para probar
+        // ---------------------------------
+
+        // se crea la instancia de director
         Director director = new Director();
 
+        // creamos una instancia de builder
         CredencialBuilder builder = new CredencialBuilder();
+        // director decide que credencial se construye
         director.construirCredencialAlumno(builder);
-
+        // tomamos la credencial desde el objeto builder anterior
         Credencial credencial = builder.getResultado();
 
+        // creamos otro objeto builder para crdencial de maestro
         CredencialBuilder builderMaestro = new CredencialBuilder();
+        // el director decide que construir
         director.construirCredencialDocente(builderMaestro);
-
+        // obtenemos el resultado desde el objeto builder
         Credencial credencialMaestro = builderMaestro.getResultado();
-
+        // imprimimos los valores de los objetos creados 
         System.out.println(credencial.getNumeroTrabajador());
         System.out.println(credencial.getCarrera());
 
