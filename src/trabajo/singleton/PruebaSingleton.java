0aeaf4d22b2a2326956ed496d3831998d8c41d10
EsePapuMisterioso8 <elpapafrancisco88@gmail.com>
2026-05-09 19:46:48 -0600

    Comentarios PruebaSingleton

--- src/trabajo/singleton/PruebaSingleton.java
+++ src/trabajo/singleton/PruebaSingleton.java
@@ -2,9 +2,22 @@ package trabajo.singleton;
 
 public class PruebaSingleton {
     public static void main(String[] args) {
+        // Ejemplos de la ejecución para Singleton
+
+        // -----------------------------------------------------------
+        // Se crean 3 instancias Singleton con diferentes parametros
+        // -----------------------------------------------------------
         Singleton singleton1 = Singleton.getInstance("localhost", null);
         Singleton singleton2 = Singleton.getInstance("miaXD","basedatos");
         Singleton singleton3 = Singleton.getInstance("TuyaXD", "basedato4");
+
+        //-------------------------------------------------------------
+        // Salidas del programa
+        // Nota: se debe de considerar que al ser el mismo objeto
+        // global, los valores de las 3 instancias deben de ser el mismo
+        // para todos y en la comparación debe de ser verdadera porque
+        // es el mismo objeto
+        //------------------------------------------------------------
         System.out.println(singleton1.getUrl());
         System.out.println(singleton2.getUrl());
         System.out.println(singleton3.getUrl());
